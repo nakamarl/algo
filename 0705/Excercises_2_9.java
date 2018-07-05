be5cb19175ff238b141599cc833ad6d2811d7585
s17007 <s17007@std.it-college.ac.jp>
2018-07-05 14:29:29 +0900

    07/05 演習2-9

---
+++ 0705/Excercises_2_9.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Excersises_2_9{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int flag;
+		
+		while(true){
+			System.out.printf("年：");
+			int year = scn.nextInt();
+			System.out.printf("月：");
+			int month = scn.nextInt();
+			System.out.printf("日：");
+			int day = scn.nextInt();
+			
+			System.out.printf("残り%d日\n",leftDayOfYear(year,month,day));
+			
+			System.out.printf("もう一度？(1:はい/0:いいえ)：");
+			flag = scn.nextInt();
+			if(flag == 0) break;
+		}
+	}
+	
+	static int leftDayOfYear(int y,int m,int d){
+		int[][] days = {
+			{31,28,31,30,31,30,31,31,30,31,30,31},
+			{31,29,31,30,31,30,31,31,30,31,30,31},
+		};
+		int lastday = 0;
+		int isUruu = (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) ? 1 : 0;
+		
+		for(int i = m;i<=11;i++){
+			lastday += days[isUruu][i];
+		}
+		lastday += days[isUruu][m-1] - d;
+		return lastday;
+	}
+}
